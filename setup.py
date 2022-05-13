043059a3c0725bbdfccfabaa5657d8770dea9a52
Zuzanna Balewski <zzbalews@gmail.com>
2022-05-12 22:28:45 -0700

    added requirements to setup; updated v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import sys
 from setuptools import setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = "AY250 Final project: ephys processing pipeline."
 
 CLASSIFIERS = list(filter(None, map(str.strip,
@@ -35,5 +35,12 @@ setup(
         platforms=['any'],
         setup_requires=['pytest-runner'],
         tests_require=['pytest'],
-        install_requires=[]
+        install_requires=['h5py==3.6.0',
+                          'mat73==0.59',
+                          'numpy==1.21.6',
+                          'pandas==1.3.5',
+                          'python-dateutil==2.8.2',
+                          'pytz==2022.1',
+                          'scipy==1.7.3',
+                          'six==1.16.0']
 )
